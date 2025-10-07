553e90af16effcfd652b6b7c4391ee62f4301580
ramkumare <ramkumarelavazhagan@gmail.com>
2025-10-08 00:57:48 +0530

    DEVELOPMENT: Ruff Formatting.

--- src/bfs_executor/process.py
+++ src/bfs_executor/process.py
@@ -7,27 +7,44 @@ from .exceptions import BFSExecutorError
 
 def run_process(
     cmd,
-    is_shell_cmd=False,
-    print_output=True,
+    is_shell_cmd=None,
+    print_output=None,
     output_stream=None,
     error_stream=None,
-    raise_exception=True,
-    convert_response_text=True,
+    raise_exception=None,
+    convert_response_text=None,
 ):
     """
     Runs the given cmd Sequence in the process and wait to complete.
+
+    Args:
+        cmd (list): Command sequence to run in the process.
+        is_shell_cmd (bool, optional): If True, the command will be executed through the shell.
+            Defaults to False.
+        print_output (bool, optional): If True, the output will be printed to the console.
+            Defaults to False.
+        output_stream (file-like object, optional): Stream to which standard output will be directed.
+            Defaults to None.
+        error_stream (file-like object, optional): Stream to which standard error will be directed.
+            Defaults to None.
+        raise_exception (bool, optional): If True, exceptions will be raised for non-zero
+
     Return: subprocess.CompletedProcess Object.
+
     """
     if not isinstance(cmd, list):
+        msg = (
+            f"args value for 'cmd' parameter must be a 'list' type but got {cmd.type()}"
+        )
         raise BFSExecutorError(
-            f"args value for 'cmd' parameter must be a 'list' type but got {cmd.type()}",
+            msg,
         )
 
     args = {
         "args": cmd,
-        "shell": is_shell_cmd,
-        "check": raise_exception,
-        "text": convert_response_text,
+        "shell": is_shell_cmd or False,
+        "check": raise_exception or True,
+        "text": convert_response_text or True,
         "universal_newlines": True,
     }
     if print_output:
